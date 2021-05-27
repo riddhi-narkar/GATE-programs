5dec6ce627efe10bb551b37319064c371991f062
grey-hat2003 <62413157+grey-hat2003@users.noreply.github.com>
2021-05-27 18:56:33 +0530

    added string and printf code

---
+++ printf_string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    char *P = "GATE";
+    char a[] = "ACADEMY";
+
+    printf("%s", P);
+    printf(P);
+
+    printf("%s", a);
+    printf(a);
+    
+    return 0;
+}
---
+++ string_rom.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Riddhi");
+    char *P = "Gate";
+
+    printf(P-7);
+
+    return 0;
+}
