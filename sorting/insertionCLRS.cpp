42301088383fe0e5befe3d1e60d8297a59b3683d
Riddhi Narkar <62413157+grey-hat2003@users.noreply.github.com>
2022-01-19 11:25:24 +0530

    added OS

--- sorting/insertionCLRS.cpp
+++ sorting/insertionCLRS.cpp
@@ -1,4 +1,4 @@
-#include<bits:stdc++.h>
+#include<bits_stdc++.h>
 using namespace std;
  
 int main()
--- string_rom.c
+++ string_rom.c
@@ -4,7 +4,7 @@ int main()
 {
     printf("Riddhi");
     char *P = "Gate";
-
+    printf("\nThe following line is printed due to ROM storage\n");
     printf(P-7);
 
     return 0;
